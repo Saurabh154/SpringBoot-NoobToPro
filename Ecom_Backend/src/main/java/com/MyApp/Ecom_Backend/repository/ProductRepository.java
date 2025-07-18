5817bcdcfb70f829308a7d3250a567e61439006d
Saurabh Kumar <106842347+Saurabh154@users.noreply.github.com>
2025-07-18 18:56:43 +0530

    Backend for product management using spring boot

---
+++ Ecom_Backend/src/main/java/com/MyApp/Ecom_Backend/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.MyApp.Ecom_Backend.repository;
+
+import com.MyApp.Ecom_Backend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    // JPQL -
+    @Query("SELECT p from Product p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(String keyword);
+
+}
---
+++ Ecom_Backend/src/main/java/com/MyApp/Ecom_Backend/service/ProductService.java
@@ -0,0 +1,48 @@
+package com.MyApp.Ecom_Backend.service;
+
+import com.MyApp.Ecom_Backend.model.Product;
+import com.MyApp.Ecom_Backend.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProduct(int id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageData(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return productRepository.save(product);
+    }
+
+
+    public void deleteProduct(int id) {
+        productRepository.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return productRepository.searchProducts(keyword);
+    }
+}
